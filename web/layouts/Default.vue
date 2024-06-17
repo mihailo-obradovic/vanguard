ad6d14f31b82be0760d89a2618b5578f270cf851
Mihailo Obradović <mihailo.obradovic.95@gmail.com>
2024-06-17 14:57:27 +0200

    Create a default layout and pages

---
+++ web/layouts/Default.vue
@@ -0,0 +1,35 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div>
+    <ul>
+      <li>
+        <NuxtLink to="/home">Home</NuxtLink>
+      </li>
+
+      <li>
+        <NuxtLink to="/register">Register</NuxtLink>
+      </li>
+
+      <li>
+        <NuxtLink to="/login">Login</NuxtLink>
+      </li>
+
+      <li>
+        <NuxtLink to="/auth-only">Auth Only</NuxtLink>
+      </li>
+
+      <li>
+        <NuxtLink to="/guest-only">Guest Only</NuxtLink>
+      </li>
+
+      <li>
+        <NuxtLink to="/logout">Logout</NuxtLink>
+      </li>
+    </ul>
+
+    <slot />
+  </div>
+</template>
+
+<style scoped></style>

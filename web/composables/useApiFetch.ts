86cd29c935d583675444ad6468a8c03c692acc06
Mihailo Obradović <mihailo.obradovic.95@gmail.com>
2024-06-18 08:56:21 +0200

    Implement login with hardcoded credentials

---
+++ web/composables/useApiFetch.ts
@@ -0,0 +1,25 @@
+import type { UseFetchOptions } from 'nuxt/app';
+
+export function useApiFetch<T>(
+  path: string | (() => string),
+  options: UseFetchOptions<T> = {}
+) {
+  let headers: any = {};
+
+  const token = useCookie('XSRF-TOKEN');
+
+  if (token.value) {
+    headers['X-XSRF-TOKEN'] = token.value as string;
+  }
+
+  // TODO: Replace with a dynamic base URL variable
+  return useFetch('http://localhost:8000' + path, {
+    credentials: 'include',
+    watch: false,
+    ...options,
+    headers: {
+      ...headers,
+      ...options?.headers
+    }
+  });
+}
--- web/pages/Login.vue
+++ web/pages/Login.vue
@@ -1,7 +1,37 @@
-<script lang="ts" setup></script>
-
 <template>
-  <div>Page: foo</div>
+  <form @submit.prevent="handleLogin">
+    <label for="email">
+      Email
+      <input id="email" type="email" v-model="form.email" />
+    </label>
+
+    <label for="password">
+      Password
+      <input id="password" type="password" v-model="form.password" />
+    </label>
+
+    <button>Login</button>
+  </form>
 </template>
+
+<script lang="ts" setup>
+const form = ref({
+  email: 'test@example.com',
+  password: 'gmaz'
+});
+
+async function handleLogin() {
+  await useApiFetch('/sanctum/csrf-cookie');
+
+  await useApiFetch('/login', {
+    method: 'POST',
+    body: form.value
+  });
+
+  const { data } = await useApiFetch('/api/user');
+
+  console.log(data);
+}
+</script>
 
 <style scoped></style>

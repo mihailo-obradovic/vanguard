195a7e43fe053ed985d23f5d71edfda0e5c3e01d
Mihailo Obradović <mihailo.obradovic.95@gmail.com>
2024-08-02 15:30:22 +0200

    Refine theme switcher

--- web/layouts/Default.vue
+++ web/layouts/Default.vue
@@ -16,9 +16,25 @@
       <v-spacer />
 
       <div class="d-flex align-center pa-2 ga-2">
-        <v-btn icon @click="toggleTheme">
-          <v-icon>{{ themeIcon }}</v-icon>
-        </v-btn>
+        <v-switch
+          :model-value="isDark"
+          color="background"
+          hide-details
+          inset
+          @change="toggleTheme"
+        >
+          <template #track-true>
+            <div class="pe-1">
+              <v-icon size="24">mdi-moon-waxing-crescent</v-icon>
+            </div>
+          </template>
+
+          <template #track-false>
+            <div class="ps-1">
+              <v-icon size="24">mdi-white-balance-sunny</v-icon>
+            </div>
+          </template>
+        </v-switch>
 
         <template v-if="auth.isLoggedIn">
           <v-btn prepend-icon="mdi-account" to="/profile">Profile</v-btn>
@@ -78,11 +94,18 @@ const theme = useTheme();
 
 const isDark = computed(() => theme.global.current.value.dark);
 
-const themeIcon = computed(() =>
-  isDark.value ? 'mdi-white-balance-sunny' : 'mdi-moon-waxing-crescent'
-);
-
 function toggleTheme() {
   theme.global.name.value = isDark.value ? 'light' : 'dark';
 }
 </script>
+
+<style scoped lang="scss">
+:deep(.v-switch__thumb) {
+  height: 18px !important;
+  width: 18px !important;
+  transform: scale(1) !important;
+}
+
+:deep(.v-switch__track-true, .v-switch__track-false) {
+}
+</style>

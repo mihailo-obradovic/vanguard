2b094a02b647d8533675a0f6fc7d7f2401263189
Mihailo Obradović <mihailo.obradovic.95@gmail.com>
2024-08-13 09:01:59 +0200

    Implement loading spinner

---
+++ web/components/shared/LoadingSpinner.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="w-100 h-100 d-flex justify-center align-center">
+    <v-progress-circular :indeterminate="isLoading" color="primary" />
+  </div>
+</template>
+
+<script setup>
+const isLoading = defineModel({
+  type: Boolean,
+  required: true
+});
+</script>

6313d4bbe9e389ba2ba828981c9712a3a69d9019
Mihailo Obradović <mihailo.obradovic.95@gmail.com>
2024-08-16 10:39:55 +0200

    Add empty layout

---
+++ web/layouts/Empty.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-layout class="w-100 h-100">
+    <v-main class="w-100 h-100">
+      <v-container
+        fluid
+        class="w-100 h-100 d-flex flex-column justify-center align-center"
+      >
+        <slot />
+      </v-container>
+    </v-main>
+  </v-layout>
+</template>
+
+<style lang="scss" scoped>
+html {
+  height: 100%;
+}
+</style>

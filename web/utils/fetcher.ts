7abaedfb281d5f7aab7fcb8b8800fbd46e500b7d
Mihailo Obradović <mihailo.obradovic.95@gmail.com>
2024-11-20 15:23:36 +0100

    Extend fetcher

--- web/utils/fetcher.ts
+++ web/utils/fetcher.ts
@@ -4,18 +4,20 @@ export function fetcher(path: string, params: any = {}) {
   const headers: any = {};
   const options: any = { ...params };
 
-  const altersServerState = ['POST', 'PUT', 'PATCH', 'DELETE'].includes(
+  const isAlteringServerState = ['POST', 'PUT', 'PATCH', 'DELETE'].includes(
     String(params?.method).toUpperCase()
   );
 
   // TODO: Check when to send the XSRF token
-  if (!params?.method || altersServerState) {
+  if (!params?.method || isAlteringServerState) {
     const token = useCookie('XSRF-TOKEN');
 
-    if (token) {
-      headers['X-XSRF-TOKEN'] = token.value;
-      options.credentials = 'include';
-    }
+    // if (!token) {
+    //   return handleError()
+    // }
+
+    headers['X-XSRF-TOKEN'] = token.value;
+    options.credentials = 'include';
   }
 
   options.headers = {
@@ -25,3 +27,21 @@ export function fetcher(path: string, params: any = {}) {
 
   return $fetch(apiBaseUrl + path, options);
 }
+
+// Idea: Use a CSRF token for guest methods
+// Idea: Use an access token for user methods
+// Idea: Use a refresh token for refreshing the access token
+// const routeRequiresAccessToken...
+// const routeRequiresRefreshToken...
+// if (routeRequiresAccessToken) {
+//   if (!accessToken){
+//     handleError()...
+//   }
+//   headers['Authorization'] = `Bearer ${accessToken}`
+// } else if (routeRequiresCsrfToken) {
+//   if (!csrfToken){
+//     handleError()...
+//   }
+//   headers['X-CSRF-TOKEN'] = csrfToken
+//   options.credentials = 'include'
+// }

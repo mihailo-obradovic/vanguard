f34e43ed1963c7196558fc5170782d85380ce01a
Mihailo Obradović <mihailo.obradovic.95@gmail.com>
2024-11-26 12:42:27 +0100

    Simplify color settings

--- web/plugins/vuetify.ts
+++ web/plugins/vuetify.ts
@@ -3,6 +3,18 @@ import '@mdi/font/css/materialdesignicons.css';
 import 'vuetify/styles';
 import { createVuetify } from 'vuetify';
 
+const baseColors = {
+  primary: '#bd93f9',
+  secondary: '#44475a',
+  accent: '#ff79c6',
+  error: '#ff5555',
+  warning: '#ffb86c',
+  info: '#8be9fd',
+  success: '#50fa7b',
+  link: '#6272a4',
+  highlight: '#f1fa8c'
+};
+
 const options = {
   theme: {
     defaultTheme: 'light',
@@ -12,17 +24,9 @@ const options = {
         isDark: false,
 
         colors: {
-          primary: '#bd93f9',
-          secondary: '#44475a',
-          accent: '#ff79c6',
-          error: '#ff5555',
-          warning: '#ffb86c',
-          info: '#8be9fd',
-          success: '#50fa7b',
           background: '#f8f8f2',
           foreground: '#282a36',
-          link: '#6272a4',
-          highlight: '#f1fa8c'
+          ...baseColors
         }
       },
 
@@ -30,17 +34,9 @@ const options = {
         isDark: true,
 
         colors: {
-          primary: '#bd93f9',
-          secondary: '#44475a',
-          accent: '#ff79c6',
-          error: '#ff5555',
-          warning: '#ffb86c',
-          info: '#8be9fd',
-          success: '#50fa7b',
           background: '#282a36',
           foreground: '#f8f8f2',
-          link: '#6272a4',
-          highlight: '#f1fa8c'
+          ...baseColors
         }
       }
     }

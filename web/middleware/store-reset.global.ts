18af292f4339caec60b97d538b0b48f8ab0b2690
Mihailo Obradović <mihailo.obradovic.95@gmail.com>
2024-12-13 15:24:36 +0100

    Undo some changes before merge

--- web/middleware/store-reset.global.ts
+++ web/middleware/store-reset.global.ts
@@ -1,7 +1,5 @@
 export default defineNuxtRouteMiddleware(() => {
   const { clearLoadingStates } = useLoadingStore();
-  const { clearFormErrors } = useFormErrorsStore();
 
   clearLoadingStates();
-  clearFormErrors();
 });
